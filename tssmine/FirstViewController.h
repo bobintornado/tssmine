824f501948377770100ba747c97a585af0da78da
Bob <bobintornado@gmail.com>
2013-11-02 23:25:34 +0800

    Initial Commit

---
+++ tssmine/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  tssmine
+//
+//  Created by Bob Cao on 2/11/13.
+//  Copyright (c) 2013 Bob Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ tssmineTests/tssmineTests.m
@@ -0,0 +1,34 @@
+//
+//  tssmineTests.m
+//  tssmineTests
+//
+//  Created by Bob Cao on 2/11/13.
+//  Copyright (c) 2013 Bob Cao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tssmineTests : XCTestCase
+
+@end
+
+@implementation tssmineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
